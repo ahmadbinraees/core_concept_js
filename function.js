91d1eec4c90e506a2be715790b9f60d158f2ee7f
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-05-04 00:43:30 +0600

    simply a fuction declared

--- function.js
+++ function.js
@@ -1,1 +1,13 @@
 /* -----------------declare a function ---------------- */
+
+function turnOn() {
+    firstWork = "1- Get up ,";
+    secWork = "2- Go to switch board ,";
+    thirdWork = "3- Turn Switch on ,";
+    lastWork = "4- Get back to your position"
+    console.log(firstWork,secWork,thirdWork,lastWork);
+}
+
+turnOn();
+
+
