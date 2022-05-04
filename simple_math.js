7d0cf93abcf84b25469d32a80b628bd8e26089d5
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-05-04 06:51:09 +0600

    addition funtion added

---
+++ simple_math.js
@@ -0,0 +1,12 @@
+/*-------------------------- addition function ------------------------*/
+
+let plus = addFunction(2,2);
+
+function addFunction(digit1, digit2) {
+    let addition = digit1 + digit2;
+    return addition;
+}
+
+console.log(plus);
+
+
