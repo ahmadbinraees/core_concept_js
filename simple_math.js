77aadefbbcf0131cfed6dd55bb748bdb6e2a5940
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-05-04 10:42:31 +0600

    subtraction function created

--- simple_math.js
+++ simple_math.js
@@ -2,11 +2,22 @@
 
 let plus = addFunction(2,2);
 
-function addFunction(digit1, digit2) {
-    let addition = digit1 + digit2;
+function addFunction(addDigit1, addDigit2) {
+    let addition = addDigit1 + addDigit2;
     return addition;
 }
 
 console.log(plus);
 
 
+
+/*----------------------------- subtraction ---------------------------*/
+
+let minus = subtractFunction(6 , 2);
+
+function subtractFunction(subDigit1 , subDigit2) {
+    let subtract = subDigit1 - subDigit2;
+    return subtract
+}
+
+console.log(minus);
