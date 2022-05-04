13b580e891cb5ca6404552286a12a790cdbb835a
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-05-04 06:27:33 +0600

    the prev file renamed simple math

---
+++ simple_math.js
@@ -0,0 +1,15 @@
+
+
+
+
+function countFunction(digit1) {
+    if (digit1 > 59) {
+        gradeA = "This is A Grade";
+    }
+    else {
+        unKnown = "The grade is unknown";
+    }
+}
+
+let count = countFunction(3);
+console.log(count);
