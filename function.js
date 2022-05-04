43b8e438e32dbdebf8967cd00760750e07d94e18
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-05-04 06:15:12 +0600

    return example added

--- function.js
+++ function.js
@@ -11,3 +11,12 @@ function turnOn() {
 turnOn();
 
 
+/*-------------------- return --------------------*/
+
+let x = myFunction(4, 3);  
+
+function myFunction(a, b) {
+  return a * b;             
+}
+
+console.log(x);
