a4b9f926694827778690de18c7bb54e812e1d247
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-05-05 10:21:01 +0600

    an object declared

--- object.js
+++ object.js
@@ -1,5 +1,13 @@
 // object is like variable. object contain multiple value with property;
 
 var personData = {
-    
+    firstName: "Ahmad",
+    lastName: "Bin Raees",
+    age: 24,
+    gmail: "ahmadullahbinraees@gmail.com",
+    fb: "www.facebook.com/ahmadbinraees",
+    fullName: function(){
+        return this.firstName + " " + this.lastName;
+    }
 };
+console.log(personData);
