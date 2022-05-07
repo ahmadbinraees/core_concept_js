87f17825535fa0b6374252b6c0d8c5493f1432d0
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-05-07 11:24:58 +0600

    new object added

--- object.js
+++ object.js
@@ -14,3 +14,23 @@ console.log(personData);
 
 name = personData.fullName();
 console.log(name);
+
+
+
+var someUser = {
+    firstName: "Tom",
+    lastName: "Henry",
+    age: 23,
+    phon: 80303030,
+    gmail: "henry555@gmail.com",
+    roll: 45,
+    address: "Dop Colony, street CIL, Banker Road - 2343",
+    fullName: function(){
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+console.log(someUser);
+
+var userFullName = someUser.fullName();
+console.log(userFullName);
